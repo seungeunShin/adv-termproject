95f377f13aa2703b4f2a239e92de65ada7a70694
dkscss <dkscss@naver.com>
2016-12-08 04:55:19 +0900

    Update myfind.c

--- myfind.c
+++ myfind.c
@@ -64,7 +64,7 @@ int dir_list(char *path, char *option, char *option2, char *option3){	//옵션2 
 					write(1, filepath, mystrlen(filepath));
 					write(1, "\n", 2);
 					mystrcat(filepath, "/");
-					dir_list(filepath, option, option2);
+					dir_list(filepath, option, option2, option3);
 				}
 				else if(S_ISREG(buf.st_mode)){
 					write(1, filepath, mystrlen(filepath));
@@ -120,7 +120,7 @@ int dir_list(char *path, char *option, char *option2, char *option3){	//옵션2 
 				write(1, filepath, mystrlen(filepath));
 				write(1, "\n", 2);
 				mystrcat(filepath, "/");
-				dir_list(filepath, option, option2);
+				dir_list(filepath, option, option2, option3);
 			}
 			else if(S_ISREG(buf.st_mode)){
 				write(1, filepath, mystrlen(filepath));
